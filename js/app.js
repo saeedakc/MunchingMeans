3136c801dd6eb93297a1c48911a3859d52227ab1
Chelsea <chels.thompson39@gmail.com>
2020-04-11 22:54:38 -0400

    started js file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 8
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+  
